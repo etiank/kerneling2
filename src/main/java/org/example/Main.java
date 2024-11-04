b6ffc715afaa78f5202ce85419fc25e419aab41c
etiank <144434318+etiank@users.noreply.github.com>
2024-11-04 09:07:46 +0100

    test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,10 @@
 package org.example;
 
+// ┌─────────────────────────────────────────────┐
+// │ Project by Etian Križman 89201173 2023/24 ® │
+// │ github: https://github.com/etiank           │
+// └─────────────────────────────────────────────┘
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
