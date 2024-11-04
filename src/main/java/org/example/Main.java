6aff43340f99457d6ee42c06f61482e39ef09e90
etiank <144434318+etiank@users.noreply.github.com>
2024-11-04 10:33:45 +0100

    Added Swing Form

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,14 @@
 package org.example;
+import javax.swing.*;
 
-// ┌─────────────────────────────────────────────┐
-// │ Project by Etian Križman 89201173 2023/24 ® │
-// │ github: https://github.com/etiank           │
-// └─────────────────────────────────────────────┘
+// ┌──────────────────────────────────────────────┐
+// │ Project by Etian Križman 89201173 2024/25 ®  │
+// │ github: https://github.com/etiank/kerneling2 │
+// │ The kernel image processing project reborn   │
+// └──────────────────────────────────────────────┘
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+
+
+
 }
